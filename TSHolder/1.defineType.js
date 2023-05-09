53a6f8a9904fcedfe7d623ac3485b4c44767342e
kyrieCCC <1186624948@qq.com>
2023-05-09 21:00:03 +0800

    feat: ts-defineType

---
+++ TSHolder/1.defineType.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//类型推断
+
+//String
+var data = "hello world!";
+
+//Number
+var data1 = 123;
+
+//Boolean
+var data2 = true;
