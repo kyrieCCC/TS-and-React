4688c0818a92b788413f74c469b1116e5e145c55
kyrieCCC <1186624948@qq.com>
2023-05-11 10:52:39 +0800

    feat: bastType-array

---
+++ TSHolder/2.baseType.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//1. any
+var any1 = 123;
+var any2 = "wlc";
+var any3 = true;
+//2. number
+var num1 = 123;
+//3. string
+var str1 = "wlc667";
+//4. boolean
+var boo1 = true;
+//5. Array
+//纯数字的数组类型
+var arr1 = [1, 2, 3, 4];
+var arr2 = [1, 2, 3, 4, 5];
---
+++ TSHolder/2.baseType.ts
@@ -0,0 +1,21 @@
+//ts当中的基本类型
+export {}
+
+//1. any
+let any1: any = 123
+let any2: any = "wlc"
+let any3: any = true
+
+//2. number
+let num1: number = 123
+
+//3. string
+let str1: string = "wlc667"
+
+//4. boolean
+let boo1: boolean = true
+
+//5. Array
+//纯数字的数组类型
+let arr1: number[] = [1, 2, 3, 4]
+let arr2: Array<number> = [1, 2, 3, 4, 5]
