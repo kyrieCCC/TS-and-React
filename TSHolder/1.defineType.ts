f1d44b8b415f370840fe237fec0fde79402e3847
kyrieCCC <1186624948@qq.com>
2023-05-12 16:51:15 +0800

    fix: movetoholder

--- TSHolder/1.defineType.ts
+++
@@ -1,9 +0,0 @@
-export {}
-
-
-//类型推断
-//分别根据输入的值类型
-//将三个变量类型推断为string、number和boolean
-const data = "hello world!"
-const data1 = 123
-const data2 = true
--- TSHolder/3.interfaceTest.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//初始化一个person
-var person = {
-    name: "wlc",
-    age: 18,
-    sex: 'boy',
-    hobby: "basektball"
-};
