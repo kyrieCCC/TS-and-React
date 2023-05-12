aa462da25afd6d2206a1473925a13ab584c62964
kyrieCCC <1186624948@qq.com>
2023-05-12 16:39:11 +0800

    feat: ts-interface-test

---
+++ TSHolder/3.interfaceTest.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//初始化一个person
+var person = {
+    name: "wlc",
+    age: 18,
+    sex: 'boy',
+    hobby: "basektball"
+};
