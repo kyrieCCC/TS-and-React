506834bb41c012adb4e3f4d5e0b1d6046fad941c
kyrieCCC <1186624948@qq.com>
2023-05-13 22:09:55 +0800

    feat: 类型别名

---
+++ TSHolder/6.typeAlias/6.typeAlias.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var a = "wlc";
+var b = true;
+//const c: myType = 123 //报错，因为我们定义的类型当中并没有number类型
---
+++ TSHolder/6.typeAlias/6.typeAlias.ts
@@ -0,0 +1,8 @@
+export { }
+
+//类型别名
+type myType = string | boolean
+
+const a: myType = "wlc"
+const b: myType = true
+//const c: myType = 123 //报错，因为我们定义的类型当中并没有number类型
