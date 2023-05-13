4557ac53ca2a698a7538138cfc02074fd8e3d67d
kyrieCCC <1186624948@qq.com>
2023-05-13 22:12:35 +0800

    feat: 类型断言

--- TSHolder/6.typeAlias/6.typeAlias.js
+++ TSHolder/6.typeAlias/6.typeAlias.js
@@ -3,3 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var a = "wlc";
 var b = true;
 //const c: myType = 123 //报错，因为我们定义的类型当中并没有number类型
+//类型断言
+var str = "wlc";
+var len1 = str.length; //此时直接写length没有代码提示，因为编译器不知道这是字符串类型
+console.log(len1);
+var len2 = str.length; //使用类型断言，确定这是一个字符串类型
+//此时出现代码提示
+console.log(len2);
--- TSHolder/6.typeAlias/6.typeAlias.ts
+++ TSHolder/6.typeAlias/6.typeAlias.ts
@@ -6,3 +6,12 @@ type myType = string | boolean
 const a: myType = "wlc"
 const b: myType = true
 //const c: myType = 123 //报错，因为我们定义的类型当中并没有number类型
+
+//类型断言
+const str: any = "wlc"
+const len1 = str.length //此时直接写length没有代码提示，因为编译器不知道这是字符串类型
+console.log(len1);
+
+const len2 = (str as string).length //使用类型断言，确定这是一个字符串类型
+//此时出现代码提示
+console.log(len2);
