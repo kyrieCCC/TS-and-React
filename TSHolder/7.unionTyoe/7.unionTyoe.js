92f778f00082e28229ea196d9791839befedaf8a
kyrieCCC <1186624948@qq.com>
2023-05-16 22:33:53 +0800

    feat: 联合类型

---
+++ TSHolder/7.unionTyoe/7.unionTyoe.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//1. 联合类型
+function test(params1) {
+    if (typeof params1 === 'string') {
+        return 'this is string type';
+    }
+    if (typeof params1 === "number") {
+        return 'this is number type';
+    }
+    throw new Error("invaild type");
+}
+console.log(test(123));
+console.log(test("wlc"));
+//console.log(test(true)); //报错，因为这不是指定的联合类型指定的类型之一
+//
